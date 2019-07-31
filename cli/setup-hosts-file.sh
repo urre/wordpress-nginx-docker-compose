07d7affae9b234ad4798682504dd8cd399b26cb9
F I Rabby <info@watchmydc.com>
2019-08-01 02:40:31 +0600

    add linux support

--- cli/setup-hosts-file.sh
+++ cli/setup-hosts-file.sh
@@ -22,32 +22,28 @@ if [ ${QUESTION} == "a" ]; then
 
 	HOSTS_LINE="$IP\t$HOSTNAME"
 
-	if [ -n "$(grep $HOSTNAME /etc/hosts)" ]
-			then
-					echo -e ${YELLOW}"$HOSTNAME already exists: $(grep $HOSTNAME $ETC_HOSTS) ${NC}"
-			else
-					echo -e ${GREEN}"Adding $HOSTNAME to your $ETC_HOSTS ${NC}";
-					sudo -- sh -c -e "echo '$HOSTS_LINE' >> /etc/hosts";
+	if [ -n "$(grep $HOSTNAME /etc/hosts)" ]; then
+		echo -e ${YELLOW}"$HOSTNAME already exists: $(grep $HOSTNAME $ETC_HOSTS) ${NC}"
+	else
+		echo -e ${GREEN}"Adding $HOSTNAME to your $ETC_HOSTS ${NC}"
+		sudo -- sh -c -e "echo '$HOSTS_LINE' >> /etc/hosts"
 
-					if [ -n "$(grep $HOSTNAME /etc/hosts)" ]
-							then
-									echo  -e ${GREEN}"$HOSTNAME was added succesfully \n $(grep $HOSTNAME /etc/hosts) ${NC}";
-							else
-									echo -e ${RED}"Failed to Add $HOSTNAME, Try again! ${NC}";
-					fi
+		if [ -n "$(grep $HOSTNAME /etc/hosts)" ]; then
+			echo -e ${GREEN}"$HOSTNAME was added succesfully \n $(grep $HOSTNAME /etc/hosts) ${NC}"
+		else
+			echo -e ${RED}"Failed to Add $HOSTNAME, Try again! ${NC}"
+		fi
 	fi
 
 fi
 
-
 if [ ${QUESTION} == "r" ]; then
 
-	if [ -n "$(grep $HOSTNAME /etc/hosts)" ]
-	then
-			echo -e ${GREEN}"$HOSTNAME Found in your $ETC_HOSTS, Removing now... ${NC}";
-			sudo sed -i".bak" "/$HOSTNAME/d" $ETC_HOSTS
+	if [ -n "$(grep $HOSTNAME /etc/hosts)" ]; then
+		echo -e ${GREEN}"$HOSTNAME Found in your $ETC_HOSTS, Removing now... ${NC}"
+		sudo sed -i".bak" "/$HOSTNAME/d" $ETC_HOSTS
 	else
-			echo -e ${RED}"$HOSTNAME was not found in your $ETC_HOSTS ${NC}";
+		echo -e ${RED}"$HOSTNAME was not found in your $ETC_HOSTS ${NC}"
 	fi
 
 fi
