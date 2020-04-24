409d5e8656bb42a2675871133606a9fd46e10ed7
Tobias Huber <huber@lumabit.de>
2020-04-24 22:15:56 +0200

    feat: read DOMAIN from .env

--- cli/windows scripts/create-cert.bat
+++ cli/windows scripts/create-cert.bat
@@ -1,6 +1,10 @@
 @echo off
+for /f "delims=" %%x in (..\..\.env) do (set "%%x")
+echo %DOMAIN%
 SET CERTS=..\..\certs
 del /Q/S %CERTS%
 md %CERTS%
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout %CERTS%\myapp.local.key -out %CERTS%\myapp.local.crt -config myapp.local.conf
- %CERTS%
+powershell -Command "(gc local.conf) -replace '<DOMAIN>', '%DOMAIN%' | Out-File -encoding ASCII localhost.conf"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout %CERTS%\%DOMAIN%.key -out %CERTS%\%DOMAIN%.crt -config localhost.conf 
+del localhost.conf
+pause
